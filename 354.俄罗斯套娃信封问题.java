6f9f8a8a713241ded342dba62614ff75b0b80e85
Bowie He <hbw234@outlook.com>
2022-10-09 22:01:54 +0800

    solve 354

--- 354.俄罗斯套娃信封问题.java
+++ 354.俄罗斯套娃信封问题.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
 /*
  * @lc app=leetcode.cn id=354 lang=java
  *
@@ -7,7 +12,38 @@
 // @lc code=start
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] == b[0]? b[1] - a[1] : a[0] - b[0];
+            }
+        });
+        int n = envelopes.length;
+        int[] height = new int[n];
+        for(int i = 0; i < n; i++) {
+            height[i] = envelopes[i][1];
+        }
+        return lengthOfLIS(height);
+    }
 
+    public int lengthOfLIS(int[] heights) {
+        int[] tail = new int[heights.length];
+        int res = 0;
+        for(int height: heights) {
+            int left = 0, right = res;
+            while(left < right) {
+                int mid = left + (right - left) / 2;
+                if(tail[mid] < height) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            tail[left] = height;
+            if(right == res) {
+                res++;
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
