573ebb67348e5477ab944fcdd9372a3792270d1b
Bowie He <hbw234@outlook.com>
2022-11-19 15:59:12 +0800

    add solution

---
+++ 142.环形链表-ii.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow  = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) break;
+        }
+        if(fast == null || fast.next == null) {
+            return null;
+        }
+        System.out.println("found loop");
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
