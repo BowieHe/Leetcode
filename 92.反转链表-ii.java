052b8cf6a449237fa3c3cea9d1abe6583e0039e5
Bowie He <hbw234@outlook.com>
2022-11-24 07:28:46 +0800

    add solution

--- 92.反转链表-ii.java
+++ 92.反转链表-ii.java
@@ -17,8 +17,26 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-
+       if(left == 1) {
+        return reversN(head, right);
+       } 
+       head.next = reverseBetween(head.next, left - 1, right - 1);
+       return head;
     }
-}
-// @lc code=end
 
+    ListNode dummy = null;
+
+    public ListNode reversN(ListNode head, int right) {
+        if(right == 1) {
+            dummy = head.next;
+            return head;
+        }
+
+        ListNode last = reversN(head.next, right - 1);
+        head.next.next = head;
+        head.next = dummy;
+        return last;
+    }
+
+}
+// @lc code=end 
