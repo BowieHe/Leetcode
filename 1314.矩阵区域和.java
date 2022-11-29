487ad4334844dff062276c63ad88973681d4875d
Bowie He <hbw234@outlook.com>
2022-11-29 21:09:23 +0800

    add solution

---
+++ 1314.矩阵区域和.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=1314 lang=java
+ *
+ * [1314] 矩阵区域和
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length, n = mat[0].length;
+        NumMatrix numMatrix = new NumMatrix(mat);
+        int[][] res = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                // left up corner
+                int x1 = Math.max(i - k, 0);
+                int y1 = Math.max(j - k, 0);
+                // right bottom corner
+                int x2 = Math.min(i + k, m - 1);
+                int y2 = Math.min(j + k, n - 1);
+
+                res[i][j] = numMatrix.sumRegion(x1, y1, x2, y2);
+            }
+        }
+        return res;
+    }
+
+    class NumMatrix {
+        private int[][] preSum;
+
+        public NumMatrix(int[][] matrix) {
+            int m = matrix.length, n = matrix[0].length;
+            if(m == 0 || n == 0) {
+                return;
+            }
+            preSum = new int[m + 1][n + 1];
+            for(int i = 1; i <= m; i++) {
+                for(int j = 1; j <= n; j++) {
+                    preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1]
+                    + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
+                }
+            }
+        }
+
+        public int sumRegion(int x1, int y1, int x2, int y2) {
+            return preSum[x2 + 1][y2 + 1] - preSum[x1][y2 + 1]
+            - preSum[x2 + 1][y1] + preSum[x1][y1];
+        }
+    }
+}
+// @lc code=end
+
