a6c98012069ac8ebd4ab90402b96ad76adf2df8a
Bowie He <hbw234@outlook.com>
2023-01-22 19:29:02 +0800

    Add solution

--- 869.重新排序得到-2-的幂.java
+++ 869.重新排序得到-2-的幂.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=869 lang=java
  *
@@ -6,8 +9,39 @@
 
 // @lc code=start
 class Solution {
-    public boolean reorderedPowerOf2(int n) {
 
+    boolean[] vis;
+
+    public boolean reorderedPowerOf2(int n) {
+        char[] nums = String.valueOf(n).toCharArray();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        return backTrace(nums, 0, 0);
+    }
+
+    public boolean backTrace(char[] nums, int index, Integer cur) {
+        if(index == nums.length) {
+            return canDevidedBy2(cur);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if((vis[i] || (cur == 0 && nums[i] == '0') ||(i > 0 && !vis[i - 1] &&  nums[i] == nums[i - 1]))) {
+                continue;
+            }
+
+            vis[i] = true;
+            if(backTrace(nums, index + 1, (cur * 10 + (nums[i] - '0')))) {
+                return true;
+            }
+            vis[i] = false;
+
+        }
+        return false;
+
+    }
+
+    boolean canDevidedBy2(int num) {
+        return ((num & (num - 1)) == 0);
     }
 }
 // @lc code=end
