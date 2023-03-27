34d4e924f4727bbceae57ddcecc5d146db71b662
Bowie He <hbw234@outlook.com>
2023-03-27 21:43:42 +0800

    add solution

---
+++ 45.跳跃游戏-ii.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=java
+ *
+ * [45] 跳跃游戏 II
+ */
+
+// @lc code=start
+class Solution {
+    public int jump(int[] nums) {
+        int length = 0;
+        int end = 0;
+        int maxPos = 0;
+        int step = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            maxPos = Math.max(maxPos, i + nums[i]);
+            if(i == end) {
+                step++;
+                end = maxPos;
+            }
+        }
+        return step;
+    }
+}
+// @lc code=end
+
