72ad412521d18105c64af2364841f1d57bd2aad8
bowie <bhe24@volvocars.com>
2023-07-18 17:13:16 +0800

    add solution

--- 90.子集-ii.java
+++ 90.子集-ii.java
@@ -5,8 +5,37 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
+
+    List<Integer> t = new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        Arrays.sort(nums);
+        dfs(false, 0, nums);
+
+        return res;
+    }
+
+    public void dfs(Boolean choosePre, int index, int[] nums) {
+        if(index == nums.length) {
+            res.add(new ArrayList<>(t));
+            return;
+        }
+        
+        dfs(false, index + 1, nums);
+        if(!choosePre && index > 0 && nums[index] == nums[index - 1]) {
+            return;
+        }
+        t.add(nums[index]);
+        dfs(true, index + 1, nums);
+        t.remove(t.size() - 1);
 
     }
 }
