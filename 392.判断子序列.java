a0115d1bd9f765dc9b890a48a316fbe7b8bdccae
bowie <bhe24@volvocars.com>
2024-06-12 11:05:03 +0800

    feat:add solution

--- 392.判断子序列.java
+++ 392.判断子序列.java
@@ -7,17 +7,15 @@
 // @lc code=start
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if(s.length() == 0)return true;
-        char[] ct = t.toCharArray();
-        int index = 0;
-        for(char c: ct){
-            if(s.charAt(index) == c)
-                index++;
-            if(index == s.length())
-                return true;
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
         }
-        return false;
+
+        return i == s.length();
     }
 }
 // @lc code=end
-
