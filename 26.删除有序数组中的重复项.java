7f184264d40c30056e7af894ef9188af6f9144ae
bowie <bhe24@volvocars.com>
2024-06-14 13:19:14 +0800

    feat:add solution

--- 26.删除有序数组中的重复项.java
+++ 26.删除有序数组中的重复项.java
@@ -7,20 +7,19 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0) {
+        if (nums.length == 0) {
             return 0;
         }
         int slow = 0, fast = 0;
-        while(fast < nums.length) {
-            if(nums[fast] != nums[slow]) {
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
                 slow++;
                 nums[slow] = nums[fast];
-            } 
+            }
             fast++;
-            
         }
+
         return slow + 1;
     }
 }
 // @lc code=end
-
