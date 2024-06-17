82a6cc95c66421204e5b057bf7c476532b436d94
bowie <bhe24@volvocars.com>
2024-06-17 17:11:06 +0800

    feat:add solution

--- 11.盛最多水的容器.java
+++ 11.盛最多水的容器.java
@@ -8,18 +8,21 @@
 class Solution {
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
-        int res = Integer.MIN_VALUE;
-        while(left < right) {
-            int current = Math.min(height[left], height[right]) * (right - left);
-            res = Math.max(res, current);
-            if(height[left] < height[right]) {
+        int res = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            if (area > res) {
+                res = area;
+            }
+
+            if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
+
         return res;
     }
 }
 // @lc code=end
-
