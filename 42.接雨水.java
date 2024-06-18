00decd0f7841b9954bbd4a6a0dafaa5fb32caccf
bowie <bhe24@volvocars.com>
2024-06-18 12:37:57 +0800

    feat:add solution

--- 42.接雨水.java
+++ 42.接雨水.java
@@ -11,24 +11,29 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
-        if(height.length == 0) {
+        if (height.length == 0) {
             return 0;
         }
+
         int n = height.length, res = 0;
-        int[] l_max = new int[n], r_max = new int[n];
-        l_max[0] = height[0];
-        r_max[n - 1] = height[n - 1];
-        for(int i = 1; i < n; i++) {
-            l_max[i] = Math.max(height[i], l_max[i - 1]);
+        int[] lMax = new int[n];
+        int[] rMax = new int[n];
+
+        lMax[0] = height[0];
+        rMax[n - 1] = height[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            lMax[i] = Math.max(height[i], lMax[i - 1]);
         }
-        for(int i = n - 2; i >= 0; i--) {
-            r_max[i] = Math.max(height[i], r_max[i + 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            rMax[i] = Math.max(height[i], rMax[i + 1]);
         }
-        for(int i = 1; i < n - 1; i++) {
-            res += Math.min(l_max[i], r_max[i]) - height[i];
+
+        for (int i = 1; i < n - 1; i++) {
+            res += Math.min(lMax[i], rMax[i]) - height[i];
         }
+
         return res;
     }
 }
 // @lc code=end
-
