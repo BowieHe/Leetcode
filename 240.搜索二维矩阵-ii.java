1f3017861f390e3b5ceb7bcd948dc5e06edcb66a
bowie <bhe24@volvocars.com>
2024-06-20 10:12:40 +0800

    feat:add solution

--- 240.搜索二维矩阵-ii.java
+++ 240.搜索二维矩阵-ii.java
@@ -7,21 +7,21 @@
 // @lc code=start
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       int m = matrix.length, n = matrix[0].length;
-       int i = 0, j = n - 1;
-       while(i < m && j >= 0) {
-        if(matrix[i][j] == target) {
-            return true;
+        int row = matrix.length, col = matrix[0].length;
+        int i = 0, j = col - 1;
+        while (i < row && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
 
+            if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
         }
-        if(matrix[i][j] < target) {
-            i++;
-        } else {
-            j--;
-        }
-       }
-       return false;
+        return false;
     }
+
 }
 // @lc code=end
-
