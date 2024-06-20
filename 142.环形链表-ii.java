b1c5c96ffd71a2be1fa99ad1bbe74f4898b04f3b
Bowie He <hbw234@outlook.com>
2024-06-20 15:00:39 +0800

    feat:add solution

--- 142.环形链表-ii.java
+++ 142.环形链表-ii.java
@@ -8,33 +8,38 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode fast = head, slow  = head;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if(fast == slow) break;
-        }
-        if(fast == null || fast.next == null) {
+        if (head == null) {
             return null;
         }
-        System.out.println("found loop");
-        slow = head;
-        while(slow != fast) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
             slow = slow.next;
+            if (fast == slow)
+                break;
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
             fast = fast.next;
+            slow = slow.next;
+
         }
         return slow;
     }
 }
 // @lc code=end
-
