6ea92753e26529876b56d77cc0c473a9fb098f26
Bowie He <hbw234@outlook.com>
2024-06-20 14:34:24 +0800

    feat:add solution

--- 141.环形链表.java
+++ 141.环形链表.java
@@ -11,30 +11,31 @@ import java.util.HashMap;
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-       if(head == null || head.next == null) {
-        return false;
-       }
-       ListNode slow = head, fast = head.next;
-       while(slow != fast){
-         if(fast == null || fast.next == null) {
+        if (head == null || head.next == null) {
             return false;
         }
-           fast = fast.next.next;
-           slow = slow.next; 
-       
-       }
-       return true;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return true;
     }
 }
 // @lc code=end
-
