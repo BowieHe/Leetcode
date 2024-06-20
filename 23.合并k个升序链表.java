0aeb9aac6207131d49bcf8ecc12f9698bea4eec9
Bowie He <hbw234@outlook.com>
2024-06-20 15:41:35 +0800

    feat:add solution

--- 23.合并k个升序链表.java
+++ 23.合并k个升序链表.java
@@ -10,41 +10,41 @@ import java.util.PriorityQueue;
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
 
     public ListNode mergeKLists(ListNode[] lists) {
-        // 0 is lists index, 1 is value of node
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> {
-            return a.val - b.val;
-        });
-
-        for(ListNode head: lists) {
-            if(head != null) {
-                pq.add(head);
-            }
-        }
-
         ListNode dummy = new ListNode(-1);
         ListNode res = dummy;
 
-        while(!pq.isEmpty()) {
-            ListNode pair = pq.poll();
-            System.out.print(pair.val);
-            res.next = pair;
-            if(pair.next != null) {
-                pq.add(pair.next);
+        if (lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode ln : lists) {
+            if (ln != null) {
+                pq.offer(ln);
             }
+
+        }
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            res.next = node;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+
             res = res.next;
         }
+
         return dummy.next;
     }
 }
 // @lc code=end
-
