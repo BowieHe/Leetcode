4ba241ffca5d2c299f3d2ff2c4e84aee009afd92
Bowie He <hbw234@outlook.com>
2024-06-24 12:40:46 +0800

    feat:add solution

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -8,35 +8,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
-        ListNode begin = dummy;
-        int addOne = 0, sum = 0;
-        while(l1 != null || l2 != null) {
-            int upVal = l1 == null? 0: l1.val;
-            int downVal = l2 == null? 0: l2.val;
-            sum = upVal + downVal + addOne;
-         
-            addOne = (sum / 10);
-            sum = sum % 10;
-            begin.next = new ListNode(sum);
-            begin = begin.next;
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
+        ListNode pre = dummy;
+        int extra = 0;
+        while (l1 != null || l2 != null || extra != 0) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int val = v1 + v2 + extra;
+            extra = 0;
+            if (val >= 10) {
+                val = val % 10;
+                extra = 1;
+            }
+            ListNode cur = new ListNode(val);
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            pre.next = cur;
+            pre = pre.next;
+
         }
-        if(addOne == 1) {
-            begin.next = new ListNode(1);
-        }
+
         return dummy.next;
     }
 }
 // @lc code=end
-
