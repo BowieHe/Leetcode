b0b643640496cf5b11c867fb6d64108f5a65ceb4
Bowie He <hbw234@outlook.com>
2024-06-24 13:46:21 +0800

    feat:add solution

--- 1314.矩阵区域和.java
+++ 1314.矩阵区域和.java
@@ -7,45 +7,27 @@
 // @lc code=start
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
-        int m = mat.length, n = mat[0].length;
-        NumMatrix numMatrix = new NumMatrix(mat);
-        int[][] res = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                // left up corner
-                int x1 = Math.max(i - k, 0);
-                int y1 = Math.max(j - k, 0);
-                // right bottom corner
-                int x2 = Math.min(i + k, m - 1);
-                int y2 = Math.min(j + k, n - 1);
+        int row = mat.length, col = mat[0].length;
+        int[][] res = new int[row][col];
+        int[][] dp = new int[row + 1][col + 1];
 
-                res[i][j] = numMatrix.sumRegion(x1, y1, x2, y2);
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= col; j++) {
+                dp[i][j] = mat[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
             }
         }
+
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= col; j++) {
+                int x0 = Math.max(i - k - 1, 0);
+                int x1 = Math.min(i + k, row);
+                int y1 = Math.min(j + k, col);
+                int y0 = Math.max(j - k - 1, 0);
+                res[i - 1][j - 1] = dp[x1][y1] - dp[x1][y0] - dp[x0][y1] + dp[x0][y0];
+            }
+        }
+
         return res;
-    }
-
-    class NumMatrix {
-        private int[][] preSum;
-
-        public NumMatrix(int[][] matrix) {
-            int m = matrix.length, n = matrix[0].length;
-            if(m == 0 || n == 0) {
-                return;
-            }
-            preSum = new int[m + 1][n + 1];
-            for(int i = 1; i <= m; i++) {
-                for(int j = 1; j <= n; j++) {
-                    preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1]
-                    + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
-                }
-            }
-        }
-
-        public int sumRegion(int x1, int y1, int x2, int y2) {
-            return preSum[x2 + 1][y2 + 1] - preSum[x1][y2 + 1]
-            - preSum[x2 + 1][y1] + preSum[x1][y1];
-        }
     }
 }
 // @lc code=end
