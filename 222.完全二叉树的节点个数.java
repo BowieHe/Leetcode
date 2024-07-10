73d870fd8b09f6cf9f6cd9003c7746e9af551a05
bowie <bhe24@volvocars.com>
2024-07-10 10:48:13 +0800

    feat:add solution

--- 222.完全二叉树的节点个数.java
+++ 222.完全二叉树的节点个数.java
@@ -5,44 +5,48 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-        int lh = 1, rh = 1;
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        while(left != null) {
-            left = left.left;
-            lh++;
-        }
-        while(right != null) {
-            right = right.right;
-            rh++;
-        }
+        // if (root == null) {
+        // return 0;
+        // }
 
-        if(lh == rh) {
-            return (int)Math.pow(2, lh) - 1;
-        }
+        // Deque<TreeNode> dq = new ArrayDeque<>();
+        // dq.add(root);
+        // int count = 0;
+        // while (!dq.isEmpty()) {
+        // TreeNode t = dq.poll();
+        // count++;
+        // if (t.left != null) {
+        // dq.add(t.left);
+        // }
+        // if (t.right != null) {
+        // dq.add(t.right);
+        // }
+        // }
 
-        return 1 + countNodes(root.left) + countNodes(root.right);
+        // return count;
+
+        return root == null ? 0 : countNodes(root.left) + countNodes(root.right) + 1;
     }
 }
 // @lc code=end
-
