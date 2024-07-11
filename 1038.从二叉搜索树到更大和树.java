7d70e2106eddd6d0625c5d2569b8649976bd69a1
bowie <bhe24@volvocars.com>
2024-07-11 11:59:28 +0800

    feat:add solution

--- 1038.从二叉搜索树到更大和树.java
+++ 1038.从二叉搜索树到更大和树.java
@@ -8,32 +8,32 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     int sum = 0;
+
     public TreeNode bstToGst(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return null;
         }
 
-        // int leftVal = bstToGst(root.left);
-        TreeNode rightVal = bstToGst(root.right);
+        bstToGst(root.right);
         sum += root.val;
         root.val = sum;
-        TreeNode leftVal = bstToGst(root.left);
+        bstToGst(root.left);
+
         return root;
     }
 }
 // @lc code=end
-
