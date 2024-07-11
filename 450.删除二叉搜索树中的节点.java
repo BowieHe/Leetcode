dcca1417b1cdc1d61c3db0d8fa9f24436ed0ef5f
bowie <bhe24@volvocars.com>
2024-07-11 16:12:41 +0800

    feat:add solution

--- 450.删除二叉搜索树中的节点.java
+++ 450.删除二叉搜索树中的节点.java
@@ -8,58 +8,52 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
 
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) {
+        if (root == null) {
             return null;
-        }
-
-        if(root.val > key) {
-            root.left = deleteNode(root.left, key);
-            return root;
-        } 
-        if(root.val < key) {
+        } else if (root.val < key) {
             root.right = deleteNode(root.right, key);
             return root;
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+            return root;
         }
-
-        if(root.val == key) {
-            if(root.left == null && root.right == null) {
+        if (root.val == key) {
+            if (root.left == null && root.right == null) {
                 return null;
-            }
-            if(root.right == null) {
-                return root.left;
-            }
-            if(root.left == null) {
+            } else if (root.left == null) {
                 return root.right;
-            }
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode successor = root.right;
+                while (successor.left != null) {
+                    successor = successor.left;
+                }
 
-            TreeNode successor = root.right;
-            while(successor.left != null) {
-                successor = successor.left;
+                root.right = deleteNode(root.right, successor.val);
+                successor.left = root.left;
+                successor.right = root.right;
+                return successor;
             }
-            root.right = deleteNode(root.right, successor.val);
-
-            successor.left =root.left;
-            successor.right = root.right;
-            return successor;
         }
 
         return root;
+
     }
 }
 // @lc code=end
-
