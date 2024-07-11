a0d1e03ad634044d5ab25f7de761f986eb37d792
bowie <bhe24@volvocars.com>
2024-07-11 11:32:05 +0800

    feat:add solution

--- 1373.二叉搜索子树的最大键值和.java
+++ 1373.二叉搜索子树的最大键值和.java
@@ -8,47 +8,60 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    int maxVal = 0;
+
+    int max;
+
     public int maxSumBST(TreeNode root) {
+
+        this.max = 0;
         traverse(root);
-        return maxVal;
+        return max;
     }
 
+    // [is BST(1, 0), minVal, maxVal, sum]
     int[] traverse(TreeNode root) {
-        
-        if(root == null) {
-            return new int[]{1, Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
-        } 
+        if (root == null) {
+            return new int[] { 1, Integer.MAX_VALUE, Integer.MIN_VALUE, 0 };
+        }
 
-        int[] maxLeft = traverse(root.left);
-        int[] maxRight = traverse(root.right);
+        int[] left = traverse(root.left);
+        int[] right = traverse(root.right);
 
         int[] res = new int[4];
+        if (left[0] == 1 && right[0] == 1 && root.val > left[2] && root.val < right[1]) {
+            // bst make
+            res = new int[] { 1, Math.min(root.val, left[1]), Math.max(root.val, right[2]),
+                    left[3] + right[3] + root.val };
 
-        if(maxLeft[0] == 1 && maxRight[0] == 1 && root.val > maxLeft[2] && root.val < maxRight[1]) {
-            res[0] = 1;
-            res[1] = Math.min(maxLeft[1], root.val);
-            res[2] = Math.max(maxRight[2], root.val);
-            res[3] = root.val + maxLeft[3] + maxRight[3];
-            maxVal = Math.max(maxVal, res[3]);
+            max = Math.max(max, res[3]);
         } else {
             res[0] = 0;
         }
+
+        // else if (left[0] == 1) {
+        // res = left;
+        // res[0] = 0;
+        // } else if (right[0] == 1) {
+        // res = right;
+        // res[0] = 0;
+        // } else {
+        // res = new int[] { 0, Integer.MAX_VALUE, Integer.MIN_VALUE, 0 };
+        // }
+
         return res;
     }
 }
 // @lc code=end
-
