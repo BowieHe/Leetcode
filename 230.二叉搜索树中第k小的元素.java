b3a0b7f88d8d68222b30f3e0761a056fcfb9eec3
bowie <bhe24@volvocars.com>
2024-07-11 15:23:03 +0800

    feat:add solution

--- 230.二叉搜索树中第k小的元素.java
+++ 230.二叉搜索树中第k小的元素.java
@@ -8,41 +8,65 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
 
-    int val;
-    int depth = 0;
+    // int count;
+    // int val;
+
+    // public int kthSmallest(TreeNode root, int k) {
+    // count = k;
+    // val = -1;
+
+    // traverse(root);
+    // return val;
+    // }
+
+    // void traverse(TreeNode root) {
+    // if (root == null) {
+    // return;
+    // }
+
+    // traverse(root.left);
+    // count--;
+    // if (count == 0) {
+    // val = root.val;
+    // }
+    // traverse(root.right);
+
+    // return;
+    // }
+
+    // solution 2
+    int count = 0;
 
     public int kthSmallest(TreeNode root, int k) {
-        traverse(root, k);
-        return val;
-    }
-
-    void traverse(TreeNode root, int k) {
-        if(root == null) {
-            return;
+        if (root == null) {
+            return -1;
         }
 
-        traverse(root.left, k);
-        depth++;
-        if(depth == k) {
-            val = root.val;
-            return;
+        int res = -1;
+        int left = kthSmallest(root.left, k);
+        count++;
+        if (count == k) {
+            res = root.val;
         }
-        traverse(root.right, k);
+        int right = kthSmallest(root.right, k);
+
+        res = Math.max(res, Math.max(left, right));
+
+        return res;
     }
 }
 // @lc code=end
-
