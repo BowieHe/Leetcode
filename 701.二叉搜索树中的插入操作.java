2972f744b73f73db65b3871fdf2300a6babed38e
bowie <bhe24@volvocars.com>
2024-07-15 09:58:46 +0800

    feat:add solution

--- 701.二叉搜索树中的插入操作.java
+++ 701.二叉搜索树中的插入操作.java
@@ -24,10 +24,10 @@ class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if (root == null) {
             return new TreeNode(val);
-        } else if (root.val < val) {
-            // insert
+        }
+        if (root.val < val) {
             root.right = insertIntoBST(root.right, val);
-        } else if (root.val > val) {
+        } else {
             root.left = insertIntoBST(root.left, val);
         }
 
