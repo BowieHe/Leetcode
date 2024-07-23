e49181e21ccb50873c1f7aa7ddfa11e74c901c91
bowie <bhe24@volvocars.com>
2024-07-23 12:33:10 +0800

    feat:add solution

--- 797.所有可能的路径.java
+++ 797.所有可能的路径.java
@@ -15,34 +15,67 @@ import java.util.Queue;
 class Solution {
 
     // bfs
+    /*
+     * public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+     * Queue<List<Integer>> queue = new LinkedList<>();
+     * List<List<Integer>> res = new ArrayList<>();
+     * 
+     * int size = graph.length;
+     * 
+     * queue.offer(List.of(0));
+     * while (!queue.isEmpty()) {
+     * int len = queue.size();
+     * for (int i = 0; i < len; i++) {
+     * List<Integer> q = queue.poll();
+     * 
+     * int last = q.get(q.size() - 1);
+     * if (last == size - 1) {
+     * res.add(q);
+     * continue;
+     * }
+     * 
+     * int[] nums = graph[last];
+     * for (int num : nums) {
+     * List<Integer> cur = new ArrayList<>(q);
+     * cur.add(num);
+     * queue.offer(cur);
+     * }
+     * }
+     * }
+     * 
+     * return res;
+     * }
+     */
+
+    // dfs
+    List<List<Integer>> res;
+
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        Queue<List<Integer>> queue = new LinkedList<>();
-        List<List<Integer>> res = new ArrayList<>();
+        res = new ArrayList<>();
 
-        int size = graph.length;
-
-        queue.offer(List.of(0));
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                List<Integer> q = queue.poll();
-
-                int last = q.get(q.size() - 1);
-                if (last == size - 1) {
-                    res.add(q);
-                    continue;
-                }
-
-                int[] nums = graph[last];
-                for (int num : nums) {
-                    List<Integer> cur = new ArrayList<>(q);
-                    cur.add(num);
-                    queue.offer(cur);
-                }
+        LinkedList<Integer> list = new LinkedList<>() {
+            {
+                add(0);
             }
+        };
+        dfs(graph, 0, list);
+        return res;
+
+    }
+
+    void dfs(int[][] graph, int index, LinkedList<Integer> l) {
+        if (index == graph.length - 1) {
+            res.add(new LinkedList<>(l));
+            return;
         }
 
-        return res;
+        int[] nums = graph[index];
+        for (int num : nums) {
+            l.addLast(num);
+            dfs(graph, num, l);
+            l.removeLast();
+        }
     }
+
 }
 // @lc code=end
