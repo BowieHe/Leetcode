39fa277fd29db745f15a9852cf904ca6271cb70d
bowie <bhe24@volvocars.com>
2024-07-24 10:25:32 +0800

    feat:add solution

--- 886.可能的二分法.java
+++ 886.可能的二分法.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import java.util.List;
 
 /*
  * @lc app=leetcode.cn id=886 lang=java
@@ -12,45 +12,56 @@ import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 // @lc code=start
 class Solution {
+
     public boolean possibleBipartition(int n, int[][] dislikes) {
+        List<Integer>[] graph = build(n, dislikes);
         int[] group = new int[n + 1];
-        List<Integer>[] graph = new List[n + 1];
-        for(int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for(int[] dislike: dislikes) {
-            int i = dislike[0];
-            int j = dislike[1];
-            graph[i].add(j);
-            graph[j].add(i);
-        }
+        Arrays.fill(group, 0);
 
-        // for(List g: graph) {
-            // System.out.println(g.toString());
-        // }
+        boolean valid = true;
 
-        for(int i = 1; i <= n; i++) {
-            if(group[i] == 0 && !dfs(graph, i, 1, group)) {
-                return false;
+        for (int i = 1; i <= n && valid; i++) {
+            if (group[i] == 0) {
+
+                valid = dfs(graph, i, 1, group);
             }
+
         }
+
+        return valid;
+    }
+
+    boolean dfs(List<Integer>[] graph, int index, int color, int[] group) {
+        if (group[index] == color) {
+            return true;
+        } else if (group[index] == (3 - color)) {
+            return false;
+        }
+
+        group[index] = color;
+        List<Integer> nums = graph[index];
+        for (int num : nums) {
+            if (!dfs(graph, num, (3 - color), group))
+                return false;
+        }
+
         return true;
     }
 
-    boolean dfs(List<Integer>[] graph, int level, int color, int[] group) {
-        if(group[level] != 0) {
-            return group[level] == color;
+    List<Integer>[] build(int n, int[][] dislikes) {
+        List<Integer>[] graph = new LinkedList[n + 1];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new LinkedList<>();
         }
 
-        group[level] = color;
-        // System.out.println(group.toString());
-        for(int pos: graph[level]) {
-            if(!dfs(graph, pos, -color, group)) {
-                return false;
-            }
+        for (int[] dis : dislikes) {
+            int a = dis[0];
+            int b = dis[1];
+            graph[a].add(b);
+            graph[b].add(a);
         }
-        return true;
+
+        return graph;
     }
 }
 // @lc code=end
-
