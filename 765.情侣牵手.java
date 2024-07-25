4056b62421b557e68ba89521a984fd78b3545560
Bowie He <hbw234@outlook.com>
2024-07-25 14:25:43 +0800

    feat:add solution

--- 765.情侣牵手.java
+++ 765.情侣牵手.java
@@ -5,52 +5,36 @@
  */
 
 // @lc code=start
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int minSwapsCouples(int[] row) {
-        int len = row.length;
-        UnionFind uf = new UnionFind(len / 2);
-        for(int i = 0; i < len; i += 2) {
-            uf.union((row[i] / 2), (row[i + 1] / 2));
+        int count = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < row.length; i++) {
+            m.put(row[i], i);
         }
 
-        return (len / 2) - uf.getCount();
-    }
+        for (int i = 0; i < row.length; i += 2) {
+            int required = row[i] % 2 == 1 ? row[i] - 1 : row[i] + 1;
+            int index = m.get(required);
+            if (index - i == 1) {
+                // already match
+                continue;
+            } else {
+                int preVal = row[i + 1];
+                row[i + 1] = required;
+                m.put(required, i + 1);
 
-    class UnionFind{ 
-        int[] parent;
-        int count;
-
-        public int getCount() {
-            return count;
-        }
-
-        public UnionFind(int n) {
-            this.count = n;
-            parent = new int[n];
-            for(int i = 0; i < n; i++) {
-                parent[i] = i;
+                row[index] = preVal;
+                m.put(preVal, index);
+                count++;
             }
         }
 
-        public void union(int x, int y) {
-            int rootx = find(x);
-            int rooty = find(y);
-            if(rootx == rooty) {
-                return;
-            }
-
-            parent[rootx] = rooty;
-            count--;
-        }
-
-        public int find(int x) {
-            while(x != parent[x]) {
-                parent[x] = parent[parent[x]];
-                x = parent[x];
-            }
-            return x;
-        }
+        return count;
     }
 }
 // @lc code=end
-
