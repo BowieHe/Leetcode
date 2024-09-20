2deff262b85088b140368430060fec24bfe9f5f9
Bowie He <hbw234@outlook.com>
2024-09-20 11:49:08 +0800

    feat:add solution

--- 743.网络延迟时间.java
+++ 743.网络延迟时间.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -11,70 +12,42 @@ import java.util.Queue;
 
 // @lc code=start
 class Solution {
+
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<int[]>[] graph = new LinkedList[n + 1];
-        for(int i = 0; i < n + 1; i++) {
-            graph[i] = new LinkedList<>();
-        }
-        for(int[] time: times) {
-            graph[time[0]].add(new int[]{time[1], time[2]});
-
+        final int INF = Integer.MAX_VALUE / 2;
+        int[][] map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(map[i], INF);
         }
 
-        int[] res = dijkstra(graph, n, k);
-
-        int maxVal = -1;
-        for(int a: res) {
-            if(a == Integer.MAX_VALUE) {
-                return -1;
-            }
-            maxVal = Math.max(a, maxVal);
+        for (int[] t : times) {
+            int u = t[0] - 1, v = t[1] - 1;
+            map[u][v] = t[2];
         }
-        return maxVal;
-    }
 
-    class State {
-        int num;
-        int distance;
+        int[] dist = new int[n];
+        boolean[] used = new boolean[n];
+        Arrays.fill(dist, INF);
+        dist[k - 1] = 0;
+        Arrays.fill(used, false);
 
-        public State(int num, int distance) {
-            this.num = num;
-            this.distance = distance;
-        }
-    }
-
-    public int[] dijkstra(List<int[]>[] graph, int n, int k) {
-        int[] distances = new int[graph.length];
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[k] = 0;
-        distances[0] = -1;
-
-        State start = new State(k, 0);
-        Queue<State> pq = new PriorityQueue<>((a, b) -> {
-            return a.distance - b.distance;
-        });
-        pq.offer(start);
-        while(!pq.isEmpty()) {
-            State currState = pq.poll();
-
-            int num = currState.num;
-            int distance = currState.distance;
-            System.out.println("current node " + num + " distance: " + distance);
-
-            if(distance > distances[num]) {
-                continue;
-            }
-            for(int[] gra: graph[num]) {
-                int curDis = distances[num] + gra[1];
-
-                if(curDis < distances[gra[0]]) {
-                    pq.offer(new State(gra[0], curDis));
-                    distances[gra[0]] = curDis;
+        for (int i = 0; i < n; i++) {
+            int x = -1;
+            for (int y = 0; y < n; y++) {
+                if (!used[y] && (x == -1 || dist[y] < dist[x])) {
+                    x = y;
                 }
             }
+            used[x] = true;
+            for (int y = 0; y < n; ++y) {
+                dist[y] = Math.min(dist[y], dist[x] + map[x][y]);
+            }
         }
-        return distances;
-    }
-}
-// @lc code=end
 
+        int ans = Arrays.stream(dist).max().getAsInt();
+        return ans == INF ? -1 : ans;
+    }
+
+}
+
+// @lc code=end
