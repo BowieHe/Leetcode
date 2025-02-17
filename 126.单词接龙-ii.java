3a523ed93573b995a133c0aedfac48f385d15fec
Bowie He <hbw234@outlook.com>
2025-02-17 13:21:42 +0800

    feat:add note for RAG and LoRA

---
+++ 126.单词接龙-ii.java
@@ -0,0 +1,179 @@
+/*
+ * @lc app=leetcode.cn id=126 lang=java
+ *
+ * [126] 单词接龙 II
+ */
+
+// @lc code=start
+
+class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> res = new ArrayList<>();
+        // 因为需要快速判断扩展出的单词是否在 wordList 里，因此需要将 wordList 存入哈希表，这里命名为「字典」
+        Set<String> dict = new HashSet<>(wordList);
+        // 特殊用例判断
+        if (!dict.contains(endWord)) {
+            return res;
+        }
+
+        dict.remove(beginWord);
+
+        // 第 1 步：广度优先搜索建图
+        // 记录扩展出的单词是在第几次扩展的时候得到的，key：单词，value：在广度优先搜索的第几层
+        Map<String, Integer> steps = new HashMap<String, Integer>();
+        steps.put(beginWord, 0);
+        // 记录了单词是从哪些单词扩展而来，key：单词，value：单词列表，这些单词可以变换到 key ，它们是一对多关系
+        Map<String, List<String>> from = new HashMap<String, List<String>>();
+        int step = 1;
+        boolean found = false;
+        int wordLen = beginWord.length();
+        Queue<String> queue = new ArrayDeque<String>();
+        queue.offer(beginWord);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String currWord = queue.poll();
+                char[] charArray = currWord.toCharArray();
+                // 将每一位替换成 26 个小写英文字母
+                for (int j = 0; j < wordLen; j++) {
+                    char origin = charArray[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        charArray[j] = c;
+                        String nextWord = String.valueOf(charArray);
+                        if (steps.containsKey(nextWord) && step == steps.get(nextWord)) {
+                            from.get(nextWord).add(currWord);
+                        }
+                        if (!dict.contains(nextWord)) {
+                            continue;
+                        }
+                        // 如果从一个单词扩展出来的单词以前遍历过，距离一定更远，为了避免搜索到已经遍历到，且距离更远的单词，需要将它从 dict 中删除
+                        dict.remove(nextWord);
+                        // 这一层扩展出的单词进入队列
+                        queue.offer(nextWord);
+
+                        // 记录 nextWord 从 currWord 而来
+                        from.putIfAbsent(nextWord, new ArrayList<>());
+                        from.get(nextWord).add(currWord);
+                        // 记录 nextWord 的 step
+                        steps.put(nextWord, step);
+                        if (nextWord.equals(endWord)) {
+                            found = true;
+                        }
+                    }
+                    charArray[j] = origin;
+                }
+            }
+            step++;
+            if (found) {
+                break;
+            }
+        }
+
+        // 第 2 步：回溯找到所有解，从 endWord 恢复到 beginWord ，所以每次尝试操作 path 列表的头部
+        if (found) {
+            Deque<String> path = new ArrayDeque<>();
+            path.add(endWord);
+            backtrack(from, path, beginWord, endWord, res);
+        }
+        return res;
+    }
+
+    public void backtrack(Map<String, List<String>> from, Deque<String> path, String beginWord, String cur,
+            List<List<String>> res) {
+        if (cur.equals(beginWord)) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (String precursor : from.get(cur)) {
+            path.addFirst(precursor);
+            backtrack(from, path, beginWord, precursor, res);
+            path.removeFirst();
+        }
+    }
+
+    // public void dfs(Map<String, List<String>> route, String begin, String end,
+    // List<String> solution) {
+    // // exceed , pass
+    // if (solution.size() >= len) {
+    // return;
+    // }
+    // // this route already passed
+    // if (solution.contains(begin)) {
+    // return;
+    // }
+    // // found solution
+    // if (begin.equals(end)) {
+    // solution.add(end);
+    // if (solution.size() < len) {
+    // System.out.println("prev len: " + String.valueOf(len));
+    // // for (List<String> s : res) {
+    // // print(s);
+    // // }
+    // res = new ArrayList<>();
+
+    // len = solution.size();
+    // }
+    // res.add(new ArrayList<>(solution));
+    // solution.remove(end);
+    // return;
+    // }
+
+    // List<String> near = route.get(begin);
+
+    // solution.add(begin);
+    // for (String word : near) {
+    // dfs(route, word, end, solution);
+    // }
+    // solution.remove(begin);
+    // }
+
+    // public void print(List<String> s) {
+    // for (String x : s) {
+    // System.out.print(x + ", ");
+    // }
+    // System.out.println();
+    // }
+
+    // public void bfs(String begin, List<String> wordList, Map<String,
+    // List<String>> route) {
+    // Deque<String> queue = new ArrayDeque<>();
+    // queue.offer(begin);
+    // while (!queue.isEmpty()) {
+    // String current = queue.poll();
+    // List<String> checked = route.getOrDefault(current, new ArrayList<>());
+    // for (String word : wordList) {
+    // if (current.equals(word) || checked.contains(word)) {
+    // continue;
+    // }
+
+    // if (nearTo(word, current)) {
+    // checked.add(word);
+    // // add to word too
+    // List<String> wordChecked = route.getOrDefault(word, new ArrayList<>());
+    // wordChecked.add(current);
+    // route.put(word, wordChecked);
+
+    // queue.offer(word);
+    // }
+    // }
+
+    // route.put(current, checked);
+    // }
+    // }
+
+    // public boolean nearTo(String a, String b) {
+    // char[] a1 = a.toCharArray(), b1 = b.toCharArray();
+    // int diff = 0;
+    // for (int i = 0; i < a1.length; i++) {
+    // if (a1[i] != b1[i]) {
+    // diff++;
+    // if (diff > 1) {
+    // return false;
+    // }
+    // }
+    // }
+    // return true;
+    // }
+
+}
+// @lc code=end
