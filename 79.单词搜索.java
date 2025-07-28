3d4ece31a80647551e09ceeb5e5b18b137990637
Bowie He <hbw234@outlook.com>
2025-07-28 15:00:58 +0800

    feat:add solution

--- 79.单词搜索.java
+++ 79.单词搜索.java
@@ -13,163 +13,43 @@ import java.util.List;
 import java.util.Map;
 
 class Solution {
-    int len, len1;
+    int m, n;
 
     public boolean exist(char[][] board, String word) {
-        Map<Character, List<int[]>> record = new HashMap<>();
-        len = board.length;
-        len1 = board[0].length;
-        int[][] route = new int[len][len1];
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len1; j++) {
-                char ch = board[i][j];
-                List<int[]> l = record.getOrDefault(ch, new ArrayList<>());
-                l.add(new int[] { i, j });
-                record.put(ch, l);
+        m = board.length;
+        n = board[0].length;
+        char[] ws = word.toCharArray();
 
-                route[i][j] = 0;
-            }
-        }
-
-        char first = word.charAt(0);
-        List<int[]> positions = record.getOrDefault(first, new ArrayList<>());
-        if (positions.size() == 0) {
-            return false;
-        } else {
-            for (int[] pos : positions) {
-                route[pos[0]][pos[1]] = 1;
-                if (dfs(board, route, record, word, 1, pos[0], pos[1])) {
-                    return true;
-                } else {
-                    route[pos[0]][pos[1]] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == ws[0]) {
+                    if (dfs(board, ws, 0, i, j)) {
+                        return true;
+                    }
                 }
             }
         }
-
         return false;
     }
 
-    public boolean dfs(char[][] board, int[][] route, Map<Character, List<int[]>> record, String word, int index, int x,
-            int y) {
-        if (index == word.length()) {
+    public boolean dfs(char[][] board, char[] word, int index, int x, int y) {
+        if (index == word.length) {
             return true;
         }
-
-        char ch = word.charAt(index);
-        List<int[]> positions = record.getOrDefault(ch, new ArrayList<>());
-        if (positions.size() == 0) {
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != word[index]) {
             return false;
-        } else {
-            for (int[] pos : positions) {
-                if ((Math.abs(pos[0] - x) == 1 && pos[1] == y) || (Math.abs(pos[1] - y) == 1 && pos[0] == x)) {
-                    if (route[pos[0]][pos[1]] == 1) {
-                        // route passes, continue
-                        continue;
-                    }
-                    route[pos[0]][pos[1]] = 1;
-                    if (dfs(board, route, record, word, index + 1, pos[0], pos[1])) {
-                        return true;
-                    } else {
-                        route[pos[0]][pos[1]] = 0;
-                        continue;
-                    }
-                } else {
-                    continue;
-                }
-            }
         }
-        return false;
-    }
-    // public boolean exist(char[][] board, String word) {
 
-    // if (word.length() == 0) {
-    // return true;
-    // }
+        char temp = board[x][y];
+        board[x][y] = '#'; // 标记为已访问
 
-    // len = board.length;
-    // len1 = board[0].length;
-    // int[][] route = new int[len][len1];
-    // for (int i = 0; i < len; i++) {
-    // int[] row = new int[len1];
-    // Arrays.fill(row, 0);
-    // route[i] = row;
-    // }
+        boolean found = dfs(board, word, index + 1, x + 1, y) ||
+                        dfs(board, word, index + 1, x - 1, y) ||
+                        dfs(board, word, index + 1, x, y + 1) ||
+                        dfs(board, word, index + 1, x, y - 1);
 
-    // for (int i = 0; i < len; i++) {
-    // for (int j = 0; j < len1; j++) {
-    // if (board[i][j] == word.charAt(0)) {
-
-    // if (word.length() == 1) {
-    // return true;
-    // }
-    // route[i][j] = 1;
-    // if (dfs(board, route, word, 1, i, j)) {
-    // return true;
-    // }
-    // route[i][j] = 0;
-    // }
-    // }
-    // }
-
-    // return false;
-
-    // }
-
-    // public boolean dfs(char[][] board, int[][] route, String word, int index, int
-    // row, int col) {
-    // char ch = word.charAt(index);
-    // List<int[]> place = contains(board, ch, row, col);
-    // if (place.size() == 0) {
-    // return false;
-    // }
-
-    // for (int[] p : place) {
-    // for (int i : p) {
-    // System.out.print(i);
-    // }
-    // System.out.println();
-    // }
-    // System.out.println();
-
-    // for (int i = 0; i < len; i++) {
-    // for (int j = 0; j < len1; j++) {
-    // System.out.print(route[i][j]);
-    // }
-    // System.out.println();
-    // }
-    // for (int[] pos : place) {
-    // if ((route[pos[0]][pos[1]]) == 0) {
-    // if (index == word.length() - 1) {
-    // System.out.println("finish");
-    // return true;
-    // }
-    // route[pos[0]][pos[1]] = 1;
-    // if (dfs(board, route, word, index + 1, pos[0], pos[1])) {
-    // return true;
-    // }
-    // route[pos[0]][pos[1]] = 0;
-    // }
-    // }
-
-    // return false;
-
-    // }
-
-    public List<int[]> contains(char[][] board, char ch, int row, int col) {
-        List<int[]> points = new ArrayList<>();
-        if (row > 0 && board[row - 1][col] == ch) {
-            points.add(new int[] { row - 1, col });
-        }
-        if (col > 0 && board[row][col - 1] == ch) {
-            points.add(new int[] { row, col - 1 });
-        }
-        if (row < len - 1 && board[row + 1][col] == ch) {
-            points.add(new int[] { row + 1, col });
-        }
-        if (col < len1 - 1 && board[row][col + 1] == ch) {
-            points.add(new int[] { row, col + 1 });
-        }
-        return points;
+        board[x][y] = temp; // 恢复单元格
+        return found;
     }
 }
 // @lc code=end
